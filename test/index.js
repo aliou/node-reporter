f1f1217e027149fb1fe443af073c0e0283d57270
Aliou Diallo <code@aliou.me>
2014-03-20 18:30:44 +0100

    Initial commit.

---
+++ test/index.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+var Reporter = require('../lib/reporter');
+
+var mockConfig = {
+  directory: __dirname + '/data/'
+}
+
+describe('Reports', function() {
+  describe('#list', function(){
+    it('should list all available reports', function() {
+      var reporter = new Reporter(mockConfig)
+      var reports = reporter.list()
+
+      assert.equal(reports.length, 18, "Four reports");
+    })
+    it('should list all the reports depending on their types', function() {
+      var reporter = new Reporter(mockConfig)
+      var reports = reporter.list({ type: 'notification' })
+
+      assert.equal(reports.length, 15, "Four reports");
+    })
+    it('should list all the reports depending on their connection', function() {
+      var reporter = new Reporter(mockConfig)
+      var reports = reporter.list({ connection: 'cellular' })
+
+      assert.equal(reports.length, 4, "Four reports");
+    })
+    it('should list all available reports', function() {
+      var reporter = new Reporter(mockConfig)
+      var reports = reporter.list({ connection: 'wifi', type: 'sleep' })
+
+      assert.equal(reports.length, 1, "Four reports");
+    })
+  })
+})
