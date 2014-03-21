f780bd619a03988b5d016b1d93d5041d5de23a2f
Aliou Diallo <code@aliou.me>
2014-03-21 17:09:54 +0100

    Add tests for the list methods with callback.

--- test/index.js
+++ test/index.js
@@ -31,5 +31,29 @@ describe('Reports', function() {
 
       assert.equal(reports.length, 1, "Four reports");
     })
+    it('should list all available reports within the callback', function() {
+      var reporter = new Reporter(mockConfig)
+      var reports = reporter.list(function(err, reports) {
+        assert.equal(reports.length, 18, "Four reports");
+      })
+    })
+    it('should list all the reports depending on their types within the callback', function() {
+      var reporter = new Reporter(mockConfig)
+      var reports = reporter.list({ type: 'notification' }, function(err, reports) {
+        assert.equal(reports.length, 15, "Four reports");
+      })
+    })
+    it('should list all the reports depending on their connection within the callback', function() {
+      var reporter = new Reporter(mockConfig)
+      var reports = reporter.list({ connection: 'cellular' }, function(err, reports) {
+        assert.equal(reports.length, 4, "Four reports");
+      })
+    })
+    it('should list all available reports depending on both their connection and their type within the callback', function() {
+      var reporter = new Reporter(mockConfig)
+      var reports = reporter.list({ connection: 'wifi', type: 'sleep' }, function(err, reports) {
+        assert.equal(reports.length, 1, "Four reports");
+      })
+    })
   })
 })
